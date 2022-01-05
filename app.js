ad33ce4aaf7dab33fa9d591f2265d761e8b8645b
LongThinh20 <thinhdao202@gmail.com>
2022-01-05 20:46:36 +0700

    lab2.4

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
   next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+  console.log("In the middleware");
+  res.send('<h1>The "Add Product " Page </h1>');
+});
+
+app.use("/", (req, res, next) => {
   console.log("Inthe another middleware!!");
+  res.send("<h1>Hello from Express.js</h1>");
 });
 
 const server = htttp.createServer(app);
