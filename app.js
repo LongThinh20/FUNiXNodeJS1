051182ab89804d56001e3f53cc5340edfb764d9e
LongThinh20 <thinhdao202@gmail.com>
2022-01-05 21:20:21 +0700

    lab2.5

---
+++ app.js
@@ -0,0 +1,28 @@
+const htttp = require("http");
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button> </form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express.js</h1>");
+});
+
+const server = htttp.createServer(app);
+
+server.listen(3000);
