c491b18564a94f806d9aaeeb1480b988d34a2c7c
LongThinh20 <thinhdao202@gmail.com>
2022-01-06 17:08:28 +0700

    chia se du lieu cac req

---
+++ app.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
+app.listen(3000);
