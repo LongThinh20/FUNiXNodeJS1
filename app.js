020cc93fd685f45d342a14e93e3dbd9627d331d5
LongThinh20 <thinhdao202@gmail.com>
2022-03-09 14:44:20 +0700

    Seving Date Through Mongoose

---
+++ app.js
@@ -0,0 +1,46 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+
+const errorController = require("./controllers/error");
+
+// const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// app.use((req, res, next) => {
+//   User.findById("61e4f2e8deb408645e441fe5")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    "mongodb+srv://studynodejs:nodejs12345@cluster0.r5jho.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
