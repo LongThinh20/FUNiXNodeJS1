40f87ebe2efe99463c3fbc36ea6cb83b0cda1d7d
LongThinh20 <thinhdao202@gmail.com>
2022-03-09 12:04:13 +0700

    Fixing a Bug

---
+++ models/order.js
@@ -0,0 +1,14 @@
+// const Sequelize = require("sequelize");
+
+// const sequelize = require("../util/database");
+
+// const Oder = sequelize.define("oder", {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true
+//   }
+// });
+
+// module.exports = Oder;
